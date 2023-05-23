9bfc349c9fdf3fc61edfc2e46c8f37036012d887
Mizanur Rahman <mizanrxb@gmail.com>
2023-05-23 15:47:57 +0600

    Reconfigured for vercel deploy

--- index.js
+++ index.js
@@ -20,13 +20,21 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    client.connect((error)=>{
+      if(error){
+        console.log(error)
+        return;
+      }
+    });
     const toysCollection = client.db('toyFusion').collection('toys')
     // index
     const indexKeys = { toyName: 1 }; // Replace field1 and field2 with your actual field names
